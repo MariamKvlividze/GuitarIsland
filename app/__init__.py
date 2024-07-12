699c51632f3bb2900d812706140807f98ec62aea
MariamKvlividze <101973139+MariamKvlividze@users.noreply.github.com>
2024-07-12 14:40:04 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from extensions import app
-from routes import home, products, about, product, offer, add_product, add_offer, edit_offer, edit_product, delete_product, delete_offer, register, login, logout, search, success
+from routes import home, products, about, product, offer, add_product, add_offer, edit_offer, edit_product, delete_product, delete_offer, register, login, logout, search, add_to_cart, remove_from_cart, cart, get_cart_items, success
